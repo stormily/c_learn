be050f854881f472f96b6d1a022c40b06a65c709
stormily <773009690@qq.com>
2017-04-09 22:20:06 +0800

    changech

--- my_practice/digital_lookup.c
+++ my_practice/digital_lookup.c
@@ -5,7 +5,7 @@ int main()
 	int a[10] = {1, 34, 2, 53, 63, 45, 34, 45, 23, 4};
 	int b, n, i;
 	scanf("%d", &b);
-	for (i = 0; i <= 9; i++){
+	for (i = 0; i <= 9; i++) {
 		if (b == a[i]) {
 			n = i + 1;
 			break;
