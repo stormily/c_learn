6db486ad22fd5efc6b35f67c5d7d6c36758f81b4
stormily <773009690@qq.com>
2017-04-09 21:12:18 +0800

    space

--- my_practice/digital_lookup.c
+++ my_practice/digital_lookup.c
@@ -5,11 +5,12 @@ int main()
 	int a[10] = { 1,34,2,53,63,45,34,45,23,4 };
 	int b, n, i;
 	scanf("%d", &b);
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++){
 		if (b == a[i]) {
 			n = i+1;
 			break;
 		}
+	}
 	printf("%d\n", n);
 	return 0;
 }
