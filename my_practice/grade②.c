4859844c11b03c74c77cc8dab853286d893bb7fe
stormily <773009690@qq.com>
2017-04-21 00:10:50 +0800

    last

--- my_practice/grade②.c
+++ my_practice/grade②.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
+int average_sum(int n, int a[3][3]);
+int average_single(int n, int a[3][3]);
+
 int main()
 {
-	int average_sum(int n, int m[3][3]);
-	int average_single(int n, int m[3][3]);
-	int a[3][3], aver_sum, aver_single, i, j, x, n, sum = 0;
-	for (i = 0; i <= 2; i++)
-		for (j = 0; j <= 2; j++) {
-			scanf("%d", &x);
-			a[i][j] = x;
-		}
-	for (n = 1; n <= 3; n++) {
-		aver_sum = average_sum(n, a);
-		printf("the average of the %d student's grade = %d\n", n, aver_sum);
-	}
-	for (n = 1; n <= 3; n++) {
-		aver_single = average_single(n, a);
-		printf("the average of the %d subject's grade = %d\n", n, aver_single);
-	}
-	return 0;
+    int a[3][3];
+    int aver_sum, aver_single;
+    int i, j, x, n;
+    
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &x);
+            a[i][j] = x;
+        }
+
+    for (n = 1; n <= 3; n++) {
+        aver_sum = average_sum(n, a);
+        printf("the average of the %d student's grade = %d\n", n, aver_sum);
+    }
+    
+    for (n = 1; n <= 3; n++) {
+        aver_single = average_single(n, a);
+        printf("the average of the %d subject's grade = %d\n", n, aver_single);
+    }
+    
+    return 0;
 }
 
 int average_sum(int n, int a[3][3])
