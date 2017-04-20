41f5c05c10315e5092fe177a8333cb096d201d80
stormily <773009690@qq.com>
2017-04-20 23:53:56 +0800

    tired

--- my_practice/grade②.c
+++ my_practice/grade②.c
@@ -11,32 +11,32 @@ int main()
 	int average_sum(int n, int m[3][3]);
 	int average_single(int n, int m[3][3]);
 	for (n = 1; n <= 3; n++) {
-		aver_sum = average_sum(n, a[3][3]);
+		aver_sum = average_sum(n, a);
 		printf("the average of the %d student's grade = %d\n", n, aver_sum);
 	}
 	for (n = 1; n <= 3; n++) {
-		aver_single = average_single(n, a[3][3]);
+		aver_single = average_single(n, a);
 		printf("the average of the %d subject's grade = %d\n", n, aver_single);
 	}
 	return 0;
 }
 
-int average_sum(int n, int m[3][3])
+int average_sum(int n, int a[3][3])
 {
 	int j, sum = 0, aver_sum;
 	n = n - 1;
 	for (j = 0; j <= 2; j++)
-		sum = sum + m[j][n];
+		sum = sum + a[j][n];
 	aver_sum = sum / 3;
 	return aver_sum;
 }
 
-int average_single(int n, int m[3][3])
+int average_single(int n, int a[3][3])
 {
 	int j, sum = 0, aver_single;
 	n = n - 1;
 	for (j = 0; j <= 2; j++)
-		sum = sum + m[n][j];
+		sum = sum + a[n][j];
 	aver_single = sum / 3;
 	return aver_single;
 }
