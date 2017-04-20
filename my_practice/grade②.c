e0810512f9cb71d313667efe72140f13cbfb031b
stormily <773009690@qq.com>
2017-04-20 23:59:01 +0800

    __

--- my_practice/grade②.c
+++ my_practice/grade②.c
@@ -2,14 +2,14 @@
 
 int main()
 {
+	int average_sum(int n, int m[3][3]);
+	int average_single(int n, int m[3][3]);
 	int a[3][3], aver_sum, aver_single, i, j, x, n, sum = 0;
 	for (i = 0; i <= 2; i++)
 		for (j = 0; j <= 2; j++) {
 			scanf("%d", &x);
 			a[i][j] = x;
 		}
-	int average_sum(int n, int m[3][3]);
-	int average_single(int n, int m[3][3]);
 	for (n = 1; n <= 3; n++) {
 		aver_sum = average_sum(n, a);
 		printf("the average of the %d student's grade = %d\n", n, aver_sum);
